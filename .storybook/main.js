34986fabd10d79eef7e51159e61f9b64175b8b43
Victor Grajski <victor.grajski@gmail.com>
2021-10-12 17:07:38 -0700

    added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  "webpackFinal": (config) => {
+      const path = require('path');
+      // add monorepo root as a valid directory to import modules from
+      config.resolve.plugins.forEach((p) => {
+        if (Array.isArray(p.appSrcs)) {
+          p.appSrcs.push(path.join(__dirname, '..', '..', '..', 'storybook'));
+        }
+      });
+      return config;
+    }
+    
+}
